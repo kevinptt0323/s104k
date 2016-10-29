be0be18bcd5fc55a45a5c8241eda81d007ca4587
Kevin Lin <kevinptt0323@gmail.com>
2016-10-30 01:08:05 +0800

    add left channel selector

--- frontend/app/pages/Channel.js
+++ frontend/app/pages/Channel.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 
 import Paper from 'material-ui/Paper';
+import {List, ListItem} from 'material-ui/List';
 
 import ChatRoom from '../components/ChatRoom';
 
@@ -13,9 +14,12 @@ class Channel extends React.Component {
     const { cid="1" } = this.props.params;
     return (
       <Paper style={{display: 'flex', position: 'relative'}}>
-        <div style={{width: 300, minHeight: '100%', borderRight: '1px solid #ccc'}}>
-          Left Selection
-        </div>
+        <List style={{width: 300, minHeight: '100%', borderRight: '1px solid #ccc'}}>
+          <ListItem primaryText="OO資訊科技" secondaryText="軟體工程師" innerDivStyle={{backgroundColor: 'rgba(128,128,128,.1)'}} />
+          <ListItem primaryText="XX資訊科技" secondaryText="專案管理" />
+          <ListItem primaryText="7-11" secondaryText="工讀生" />
+          <ListItem primaryText="OX餐廳" secondaryText="特級廚師" />
+        </List>
         <div style={{flex: 1}}>
           <ChatRoom room={cid} />
         </div>
