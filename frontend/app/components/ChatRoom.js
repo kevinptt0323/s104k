020c424163153c16f63a5632a190b3503ce768aa
Kevin Lin <kevinptt0323@gmail.com>
2016-10-29 22:40:48 +0800

    add socket.io

--- frontend/app/components/ChatRoom.js
+++ frontend/app/components/ChatRoom.js
@@ -4,15 +4,24 @@ import socket from 'socket.io-client';
 class ChatRoom extends React.Component {
   constructor(props) {
     super(props);
-    const io = socket('http://localhost:8000');
+    const io = socket('http://s104k.nctu.me:8787');
     this.io = io;
+    const { cid } = props;
 
     io.on('connect', () => {
+      io.emit('join room', { room: cid, account: "XD" });
+      console.log("join");
     });
-    io.on('event', (data) => {
+    io.on('load msg', (data) => {
+      console.log(data);
+      this.setState({ message: data });
     });
-    io.on('disconnect', () => {
+    io.on('broadcast msg', (data) => {
+      console.log(data);
     });
+  }
+  componentWillUnmount() {
+    this.io.emit('leave room', { room: cid });
   }
   render() {
     const { cid } = this.props;
