93a6091ff5566e7037a67d7cca609bf4d6831b2b
JubeatWwW <l6104400@gmail.com>
2016-10-30 08:37:18 +0800

    header changed

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE');
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token");
     next();
 });
 app.post('/', (req, res)=>{
